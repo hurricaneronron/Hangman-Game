1fe567d4582fcb625a7e7945d6adee7ecad5c43f
hurricaneronron <vprzybyl@gmail.com>
2018-02-05 12:27:12 -0800

    debugged and ready to go

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,27 +6,27 @@ $( document ).ready( function ( ) {
   var guessesLeft = 0
   
   
-  $( ".btn" ).on( "click" , function ( ) {
+  $( ".btn" ).on( "click", function ( ) {
     var playWord = wordBank[ Math.floor( Math.random( ) * wordBank.length )]
     var wordDisplay = playWord.split( "" )
     for( var i = 0; i < playWord.length; i++ ) {
       wordDisplay[i] = "  _  "
     }
-    guessesLeft = 12
     var remainingLetters = playWord.length
     remainingLetters = playWord.length
+    guessesLeft = 12
     $( "#game-space" ).html( wordDisplay )
     $( "#guessesLeft" ).text( "12" )
     $( "#lettersGuessed" ).empty()
-    $( "#page-heading" ).html( "Hangman Game!" )
+    $( "#page-heading" ).html( "Doctor Who Hangman Game!" )
     console.log("playWord: " + playWord)
     console.log("RemainingLetters: " + remainingLetters)
     console.log("GuessesLeft: " + guessesLeft)
     console.log("wins: " + wins)
     console.log("Losses: " + losses)
     
-    $( document ).keyup( function ( e ) {
-      var input = String.fromCharCode(e.keyCode)
+    $( document ).on( "keyup",  function ( e )  {
+      var input = String.fromCharCode(e.which)
       console.log("Input: " + input)
       var char = input.toLowerCase()
       console.log("char: " + char)
@@ -40,12 +40,12 @@ $( document ).ready( function ( ) {
       
       if( check === true ) {
         for( var i = 0; i < playWord.length; i++ ) {
-          if (str[i] === char ) {
+          if (str[i] == char ) {
             wordDisplay[i] = char
-            $( "#game-space" ).text( wordDisplay.join( " " ) )
             remainingLetters--
           }
         }
+        $( "#game-space" ).text( wordDisplay.join( " " ) )
         console.log( "RemainingLetters: " + remainingLetters )
       }
       
@@ -59,12 +59,14 @@ $( document ).ready( function ( ) {
         $( "#page-heading" ).html( "<h1>GAME OVER</h1><h5>CLICK BUTTON TO PLAY AGAIN</h5>" )
         losses++
         $( "#losses" ).text( losses )
+        $( document ).off( "keyup" )
       }
       
       if( remainingLetters === 0 ) {
         $( "#page-heading" ).html( "<h1>YOU WIN</h1><h5>CLICK BUTTON TO PLAY AGAIN</h5>" )
         wins++
         $( "#wins" ).text( wins )
+        $( document ).off( "keyup" )
       }
     })
   })   
